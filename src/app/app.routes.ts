b68aea3dec64934a4e5d15ae6cc7526ef52c71eb
marandu3 <johnwillymarandu@gmail.com>
2025-07-24 13:40:09 +0300

    the files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,41 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Homepage } from './pages/homepage/homepage';
+import { News } from './pages/news/news';
+import { Events } from './pages/events/events';
+import { Community } from './pages/community/community';
+import { Settings } from './pages/settings/settings';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'homepage',
+        pathMatch: 'full'
+    },
+    {
+        path:'homepage',
+        component: Homepage,
+        title: 'Homepage'
+    },
+    {
+        path: 'News',
+        component:News,
+        title:' News',
+    },
+    {
+        path:'Events',
+        component: Events,
+        title: 'Events',
+    },
+    {
+        path:'Community',
+        component: Community,
+        title: 'Community',
+    },
+    {
+        path:'Settings',
+        component: Settings,
+        title: 'Settings',
+    }
+
+];
