9e5a134519974ff3d3873e5a7aae9569a5ce2876
marandu3 <johnwillymarandu@gmail.com>
2025-07-25 14:51:38 +0300

    some changes to login form and adding pages without anything register and forgot password

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  constructor(private router: Router) {}
 
+  navigateToregister() {
+    this.router.navigate(['register']);
+  }
+
+  navigateToForgotPassword() {
+    this.router.navigate(['forgot-password']);
+  }
 }
