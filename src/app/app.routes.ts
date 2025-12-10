42aa91db578cfb6b106ad27cbd1838845c711d3f
marandu3 <johnwillymarandu@gmail.com>
2025-12-10 21:44:36 +0300

    added the routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Login } from './pages/login/login';
+import { Signup } from './pages/signup/signup';
 
 export const routes: Routes = [
     {
         path: '',
         component: Home
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    },
+    {
+        path: 'about',
+        component:About
+    },
+    {
+        path: "contact",
+        component: Contact
+    },
+    {
+        path: 'login',
+        component: Login
+    },
+    {
+        path: 'signup',
+        component: Signup
     }
 ];
 
